1888be71c8538a7d7a1bc76073be083ea09f9bc8
mohamad007 <mohamad137977@gmail.com>
2016-07-10 18:07:02 +0430

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -112,7 +112,9 @@ else
   fi
   while true; do
    rm -r ../.telegram-cli/state
-   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/Firebot 1 -E $@
+   ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/Firebot.lua -l 1 -E $@
    sleep 3
   done
 fi
+
+
